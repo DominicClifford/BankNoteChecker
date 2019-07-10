6d307145ed156302fc650b03d2375e7817b5b6f1
DominicClifford <32680583+DominicClifford@users.noreply.github.com>
2019-07-11 00:44:51 +0100

    Addition of Animation to Buttons

--- app/src/main/java/com/example/banknotechecker/Page_Home.java
+++ app/src/main/java/com/example/banknotechecker/Page_Home.java
@@ -1,10 +1,13 @@
 package com.example.banknotechecker;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class Page_Home extends AppCompatActivity {
@@ -14,9 +17,14 @@ public class Page_Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_home);
 
+        //Animation Preset
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_animation);
+
         //Open England Page
         ImageView eng = findViewById(R.id.engButton);
+        eng.startAnimation(animation);
         eng.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Intent pageEng = new Intent(Page_Home.this, Page_England.class);
@@ -26,6 +34,7 @@ public class Page_Home extends AppCompatActivity {
 
         //Open Scotland page
         ImageView scot = findViewById(R.id.scotButton);
+        scot.startAnimation(animation);
         scot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
