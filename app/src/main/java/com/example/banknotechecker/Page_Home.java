53a183941368be18248c6c190c53b2e0dbc41467
DominicClifford <32680583+DominicClifford@users.noreply.github.com>
2019-07-11 00:11:49 +0100

    Linking of country pages to Home page and images

--- app/src/main/java/com/example/banknotechecker/Page_Home.java
+++ app/src/main/java/com/example/banknotechecker/Page_Home.java
@@ -1,7 +1,11 @@
 package com.example.banknotechecker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class Page_Home extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class Page_Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_home);
+
+        //Open England Page
+        ImageView eng = findViewById(R.id.engButton);
+        eng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pageEng = new Intent(Page_Home.this, Page_England.class);
+                startActivity(pageEng);
+            }
+        });
+
+        //Open Scotland page
+        ImageView scot = findViewById(R.id.scotButton);
+        scot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pageScot = new Intent(Page_Home.this, Page_Scotland.class);
+                startActivity(pageScot);
+            }
+        });
+
     }
+
+
+
+
 }
