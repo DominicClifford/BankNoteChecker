4c250a7026331b1654af1d25e3aa8f6d7818def7
DominicClifford <32680583+DominicClifford@users.noreply.github.com>
2019-08-22 21:16:52 +0100

    Addition of the £10, £20 & £50 note buttons along with the actuation code for when they are clicked.

--- app/src/main/java/com/example/banknotechecker/Page_England.java
+++ app/src/main/java/com/example/banknotechecker/Page_England.java
@@ -30,7 +30,46 @@ public class Page_England extends AppCompatActivity {
             }
         });
 
+        //Open England Page when english button is clicked
+        ImageView engTenner = findViewById(R.id.engTennerButton);
+        engTenner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set animation on click
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_animation);
+                v.startAnimation(animation);
+                //open new activity on click (Temp return to home)
+                Intent pageEngTenner = new Intent(Page_England.this, Page_Home.class);
+                startActivity(pageEngTenner);
+            }
+        });
 
+        //Open England Page when english button is clicked
+        ImageView engTwenty = findViewById(R.id.engTwentyButton);
+        engTwenty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set animation on click
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_animation);
+                v.startAnimation(animation);
+                //open new activity on click (Temp return to home)
+                Intent pageEngTwenty = new Intent(Page_England.this, Page_Home.class);
+                startActivity(pageEngTwenty);
+            }
+        });
 
+        //Open England Page when english button is clicked
+        ImageView engFifty = findViewById(R.id.engFiftyButton);
+        engFifty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set animation on click
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_animation);
+                v.startAnimation(animation);
+                //open new activity on click (Temp return to home)
+                Intent pageEngFifty = new Intent(Page_England.this, Page_Home.class);
+                startActivity(pageEngFifty);
+            }
+        });
     }
 }
