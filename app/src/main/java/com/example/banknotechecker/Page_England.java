fbe42f5e2135dd151ae43d6f19ca9e0dde230664
DominicClifford <32680583+DominicClifford@users.noreply.github.com>
2019-08-22 20:49:35 +0100

    Addition of the £5 note button to England Page

--- app/src/main/java/com/example/banknotechecker/Page_England.java
+++ app/src/main/java/com/example/banknotechecker/Page_England.java
@@ -1,7 +1,13 @@
 package com.example.banknotechecker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.view.View.OnClickListener;
 
 public class Page_England extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class Page_England extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_england);
+
+        //Open England Page when english button is clicked
+        ImageView engFiver = findViewById(R.id.engFiverButton);
+        engFiver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set animation on click
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_animation);
+                v.startAnimation(animation);
+                //open new activity on click (Temp return to home)
+                Intent pageEngFiver = new Intent(Page_England.this, Page_Home.class);
+                startActivity(pageEngFiver);
+            }
+        });
+
+
+
     }
 }
