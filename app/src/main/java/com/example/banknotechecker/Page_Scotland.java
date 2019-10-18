e9a07352357ffb1e30e444e319f9af07d11400fe
DominicClifford <32680583+DominicClifford@users.noreply.github.com>
2019-10-18 10:12:16 +0100

    Changes to layout format to make them more universal for all pages

--- app/src/main/java/com/example/banknotechecker/Page_Scotland.java
+++ app/src/main/java/com/example/banknotechecker/Page_Scotland.java
@@ -1,13 +1,22 @@
 package com.example.banknotechecker;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Page_Scotland extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_scotland);
+
+        TextView sFiveP = findViewById(R.id.SFiveP);
+        sFiveP.setPaintFlags(sFiveP.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
     }
 }
