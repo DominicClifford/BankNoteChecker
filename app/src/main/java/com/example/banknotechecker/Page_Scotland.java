f1522d748343fe192698795c987e5effeacdf8f5
DominicClifford <32680583+DominicClifford@users.noreply.github.com>
2020-01-06 17:38:24 +0000

    Button functionality and animation added to RBS fiver button on Scotland main page

--- app/src/main/java/com/example/banknotechecker/Page_Scotland.java
+++ app/src/main/java/com/example/banknotechecker/Page_Scotland.java
@@ -61,6 +61,19 @@ public class Page_Scotland extends AppCompatActivity {
             }
         });
 
+        //Open Scotland CB Five Page when button is clicked
+        ImageView sRbsFive = findViewById(R.id.sRbsFive);
+        sRbsFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set animation on click
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_animation);
+                v.startAnimation(animation);
+                //open new activity on click (Temp return to home)
+                Intent pageRbsFiver = new Intent(Page_Scotland.this, Scotland_RBS_Five.class);
+                startActivity(pageRbsFiver);
+            }
+        });
 
     }
 }
