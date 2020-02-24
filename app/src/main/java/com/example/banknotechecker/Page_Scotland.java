394494ddde2b232d3c128e3c6d7b610225d822e9
DominicClifford <32680583+DominicClifford@users.noreply.github.com>
2020-02-24 21:53:57 +0000

    CB Bank Hundred Pound Note Page added - All Drawable items added - Page linked with main Scotland Note page - Android Manifest ammended

--- app/src/main/java/com/example/banknotechecker/Page_Scotland.java
+++ app/src/main/java/com/example/banknotechecker/Page_Scotland.java
@@ -214,5 +214,19 @@ public class Page_Scotland extends AppCompatActivity {
                 startActivity(pageBOSHun);
             }
         });
+
+        //Open Scotland CB Bank Hundred Page when button is clicked
+        ImageView sCbHun = findViewById(R.id.sCbHun);
+        sCbHun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set animation on click
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_animation);
+                v.startAnimation(animation);
+                //open new activity on click (Temp return to home)
+                Intent pageCbHun = new Intent(Page_Scotland.this, Scotland_CB_Hun.class);
+                startActivity(pageCbHun);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/banknotechecker/Scotland_CB_Hun.java
@@ -0,0 +1,59 @@
+package com.example.banknotechecker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class Scotland_CB_Hun extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scotland_cb_hun);
+
+        TextView hunFront = findViewById(R.id.sCBHunFront);
+        hunFront.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView noteImage = findViewById(R.id.sCBHunImage);
+                noteImage.setImageResource(R.drawable.s_cb_hun_front);
+
+                TextView fiverBack1 = findViewById(R.id.sCBHunFront);
+                fiverBack1.setTextSize(30);
+
+                TextView fiverFront1 = findViewById(R.id.sCBHunBack);
+                fiverFront1.setTextSize(20);
+
+                ScrollView frontMain1 = findViewById(R.id.frontMain1);
+                frontMain1.setVisibility(View.VISIBLE);
+
+                ScrollView frontMain2 = findViewById(R.id.frontMain2);
+                frontMain2.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        TextView hunBack = findViewById(R.id.sCBHunBack);
+        hunBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView noteImage = findViewById(R.id.sCBHunImage);
+                noteImage.setImageResource(R.drawable.s_cb_hun_back);
+
+                TextView fiverBack1 = findViewById(R.id.sCBHunFront);
+                fiverBack1.setTextSize(20);
+
+                TextView fiverFront1 = findViewById(R.id.sCBHunBack);
+                fiverFront1.setTextSize(30);
+
+                ScrollView frontMain1 = findViewById(R.id.frontMain1);
+                frontMain1.setVisibility(View.INVISIBLE);
+
+                ScrollView frontMain2 = findViewById(R.id.frontMain2);
+                frontMain2.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
