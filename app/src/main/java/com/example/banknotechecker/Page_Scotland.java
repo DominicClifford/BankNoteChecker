b4bbecd2e6c836e5ab3d50ee77d2b5947f5804ef
DominicClifford <32680583+DominicClifford@users.noreply.github.com>
2020-02-24 14:44:50 +0000

    Bank of Scotland Fifty Pound Note Page added - All Drawable items added - Page linked with main Scotland Note page - Android Manifest ammended

--- app/src/main/java/com/example/banknotechecker/Page_Scotland.java
+++ app/src/main/java/com/example/banknotechecker/Page_Scotland.java
@@ -158,5 +158,19 @@ public class Page_Scotland extends AppCompatActivity {
                 startActivity(pageRbsTwenty);
             }
         });
+
+        //Open Scotland Bank of Scotland Fifty Page when button is clicked
+        ImageView sBosFifty = findViewById(R.id.sBosFifty);
+        sBosFifty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set animation on click
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_animation);
+                v.startAnimation(animation);
+                //open new activity on click (Temp return to home)
+                Intent pageBosFifty = new Intent(Page_Scotland.this, Scotland_BOS_Fifty.class);
+                startActivity(pageBosFifty);
+            }
+        });
     }
 }
