7d70e3a31d50863264cedcc2d21251a84ba293c6
DominicClifford <32680583+DominicClifford@users.noreply.github.com>
2020-02-24 00:05:57 +0000

    Royal Bank of Scotland Twenty Pound Note Page added - All Drawable items added - Page linked with main Scotland Note page - Android Manifest ammended

--- app/src/main/java/com/example/banknotechecker/Page_Scotland.java
+++ app/src/main/java/com/example/banknotechecker/Page_Scotland.java
@@ -144,5 +144,19 @@ public class Page_Scotland extends AppCompatActivity {
                 startActivity(pageCbTwenty);
             }
         });
+
+        //Open Scotland Royal Bank of Scotland Twenty Page when button is clicked
+        ImageView sRbsTwenty = findViewById(R.id.sRbsTwenty);
+        sRbsTwenty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set animation on click
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_animation);
+                v.startAnimation(animation);
+                //open new activity on click (Temp return to home)
+                Intent pageRbsTwenty = new Intent(Page_Scotland.this, Scotland_RBS_Twenty.class);
+                startActivity(pageRbsTwenty);
+            }
+        });
     }
 }
