88e8a25d3592cfbc1cd578575892d39f22639965
DominicClifford <32680583+DominicClifford@users.noreply.github.com>
2020-02-24 22:01:00 +0000

    Royal Bank of Scotland Bank Hundred Pound Note Page added - All Drawable items added - Page linked with main Scotland Note page - Android Manifest ammended

--- app/src/main/java/com/example/banknotechecker/Page_Scotland.java
+++ app/src/main/java/com/example/banknotechecker/Page_Scotland.java
@@ -228,5 +228,19 @@ public class Page_Scotland extends AppCompatActivity {
                 startActivity(pageCbHun);
             }
         });
+
+        //Open Scotland Royal Bank of Scotland Bank Hundred Page when button is clicked
+        ImageView sRbsHun = findViewById(R.id.sRbsHun);
+        sRbsHun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set animation on click
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_animation);
+                v.startAnimation(animation);
+                //open new activity on click (Temp return to home)
+                Intent pageRbsHun = new Intent(Page_Scotland.this, Scotland_RBS_Hun.class);
+                startActivity(pageRbsHun);
+            }
+        });
     }
 }
