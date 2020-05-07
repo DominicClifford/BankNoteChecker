8dabfc32019a31d10ec8e6f1e372d1a684b7ba06
DominicClifford <32680583+DominicClifford@users.noreply.github.com>
2020-05-07 12:15:54 +0100

    Bare Bones added to the Northern Irish notes home page - ni_boi_five_button added - Text Added to page - All spacing added and worked out

--- app/src/main/java/com/example/banknotechecker/Page_NIreland.java
+++ app/src/main/java/com/example/banknotechecker/Page_NIreland.java
@@ -1,7 +1,9 @@
 package com.example.banknotechecker;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Page_NIreland extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class Page_NIreland extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_nireland);
+
+        TextView niFiveP = findViewById(R.id.NiFiveP);
+        TextView niTenP = findViewById(R.id.NiTenP);
+        TextView niTwentyP = findViewById(R.id.NiTwentyP);
+        TextView niFiftyP = findViewById(R.id.NiFiftyP);
+        TextView niHunP = findViewById(R.id.NiHunP);
+
+
+        niFiveP.setPaintFlags(niFiveP.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        niTenP.setPaintFlags(niTenP.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        niTwentyP.setPaintFlags(niTwentyP.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        niFiftyP.setPaintFlags(niFiftyP.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        niHunP.setPaintFlags(niHunP.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+
+
     }
 }
